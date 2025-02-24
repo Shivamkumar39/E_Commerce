f8a6a25f58d7453f2f1cfe94e2b6ffdf864049fa
Shivamkumar098798 <shivamkumar098798@gmail.com>
2025-02-24 14:12:43 +0530

    First_Commet

---
+++ models/user.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    _id: {type: String, require: true},
+    name: {type: String, require: true},
+    email: {type: String, require: true, unique: true},
+    imageUrl: {type: String, require: true},
+    cartItems: {type: Object, default: {}}
+}, {minimize: false})
+const User =mongoose.models.user || mongoose.model('User', userSchema);
+
+export default User;
